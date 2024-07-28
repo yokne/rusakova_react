b9ecce611f56e535a7c2431ea5fe634a59cabf15
s.rusakova <s.rusakova@vk.team>
2024-07-28 23:01:11 +0300

    fix

--- src/components/counter/CounterContainer.jsx
+++ src/components/counter/CounterContainer.jsx
@@ -1,18 +1,9 @@
-import { useEffect } from "react";
 import { Counter } from "./Counter";
 import { useCounter } from "./useCounter";
 
 // eslint-disable-next-line react/prop-types
 export const CounterContainer = ({ value = 0, min = 0, max = 5, onChange = () => {} }) => {
-    const {increment, decrement, count, setCount} = useCounter(value, min, max);
-
-    useEffect(() => {
-        setCount(value);
-    }, [value]);
-
-    useEffect(() => {
-        onChange(count);
-    }, [count]);
+    const {increment, decrement, count } = useCounter(value, min, max, onChange);
 
     return <Counter val={count} dec={decrement} inc={increment} />;
 };
--- src/components/counter/useCounter.jsx
+++ src/components/counter/useCounter.jsx
@@ -1,18 +1,24 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
-export const useCounter = (value, min, max) => {
+export const useCounter = (value, min, max, onChange) => {
     const [count, setCount] = useState(value);
 
     const increment = () => {
         if (count < max) {
             setCount(count + 1);
+            onChange((prev) => prev++);
         }
     };
     const decrement = () => {
         if (count > min) {
             setCount(count - 1);
+            onChange((prev) => prev--);
         }
     };
 
-    return {increment, decrement, count, setCount}
-}
+    useEffect(() => {
+        setCount(value);
+    }, [value]);
+
+    return { increment, decrement, count, setCount };
+};
